757ffd5ed7557183a30baae341ad772ce4c23895
NeatuR <ne.rux.biosciences@gmail.com>
2023-07-21 14:07:14 +0100

    Kept one Main class

--- src/main/java/uk/ac/ncl/workshop_admin/Main.java
+++ src/main/java/uk/ac/ncl/workshop_admin/Main.java
@@ -1,135 +1,150 @@
 package uk.ac.ncl.workshop_admin;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import uk.ac.ncl.workshop_admin.model.Person;
+import uk.ac.ncl.workshop_admin.model.Programme;
+import uk.ac.ncl.workshop_admin.model.School;
+import uk.ac.ncl.workshop_admin.model.Stage;
+import uk.ac.ncl.workshop_admin.util.DatabaseManager;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.List;
 
-import uk.ac.ncl.workshop_admin.model.Programme;
-import uk.ac.ncl.workshop_admin.model.Stage;
-import uk.ac.ncl.workshop_admin.util.DatabaseManager;
-import uk.ac.ncl.workshop_admin.model.School;
-import uk.ac.ncl.workshop_admin.model.Person;
-
-
 public class Main {
 
-        private static final ArrayList<Person> PEOPLE_DATA = new ArrayList<>(Arrays.asList(
-            new Person("Harry", "Potter"),
-            new Person("James", "Potter"),
-            new Person("Lilly", "Potter"),
-            new Person("Hermione", "Granger"),
-            new Person("Arthur", "Weasley"),
-            new Person("Bill", "Weasley"),
-            new Person("Fred", "Weasley")
-        ));
-        private static final ArrayList<School> SCHOOL_DATA = new ArrayList<>(Arrays.asList(
-            new School("Medical School"),
-            new School("Engineering"),
-            new School("Business"),
-            new School("Law"),
-            new School("Witchcraft"),
-            new School("Real or Cake"),
-            new School("Dark Arts")
-        ));
+  public static void main(String[] args) {
+    DatabaseManager databaseManager = new DatabaseManager();
 
-        private static final ArrayList<Programme> PROGRAMME_DATA = new ArrayList<>(Arrays.asList(
-            new Programme("BSc"),
-            new Programme("CompSci"),
-            new Programme("MSc"),
-            new Programme("PhD")
-        ));
+    initDataset(databaseManager);
 
-        private static final ArrayList<Stage> STAGE_DATA = new ArrayList<>(Arrays.asList(
-            new Stage("I"),
-            new Stage("II"),
-            new Stage("III"),
-            new Stage("IV"),
-            new Stage("V"),
-            new Stage("VI"),
-            new Stage("VII"),
-            new Stage("VIII"),
-            new Stage("IX"),
-            new Stage("X")
-        ));
+    databaseManager.open();
+    printAllPeople(databaseManager.read(Person.class));
+    printAllSchools(databaseManager.read(School.class));
+    printAllProgrammes(databaseManager.read(Programme.class));
+    printAllStages(databaseManager.read(Stage.class));
+    databaseManager.close();
+  }
 
-        public static void main(String[] args) {
-            DatabaseManager databaseManager = new DatabaseManager();
+  public static void printAllPeople(List<Person> objects) {
+    System.out.println("People:");
+    for (Person obj : objects)
+      System.out.println("    " + obj.toFullString());
+  }
 
-            initDataset(databaseManager);
+  public static void printAllSchools(List<School> objects) {
+    System.out.println("Schools:");
+    for (School obj : objects)
+      System.out.println("    " + obj.toFullString());
+  }
 
-            databaseManager.open();
-            printAllPeople(databaseManager.read(Person.class));
-            printAllSchools(databaseManager.read(School.class));
-            printAllProgrammes(databaseManager.read(Programme.class));
-            printAllStages(databaseManager.read(Stage.class));
-            databaseManager.close();
+  public static void printAllProgrammes(List<Programme> objects) {
+    System.out.println("Programmes:");
+    for (Programme obj : objects)
+      System.out.println("    " + obj.toFullString());
+  }
+
+  public static void printAllStages(List<Stage> objects) {
+    System.out.println("Stages:");
+    for (Stage obj : objects)
+      System.out.println("    " + obj.toFullString());
+  }
+
+  private static void initDataset(DatabaseManager databaseManager) {
+    HashMap<String, Object> parameters;
+
+    // Load data from the CSV file
+    try (Reader reader = new FileReader("uniFormat.csv")) {
+      CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader()
+          .withHeader("ID","user profile","username","uuid","date created","date modified","source","occurrence","contact number","attended","additional notes","attended datetime","eval email sent","eval submitted","User type","Programme","Stage","School","Attended without booking?","attendance method","synced to portfolio","email"));
+
+      for (CSVRecord record : csvParser) {
+        // Get data from the CSV record
+        String userProfile = record.get("user profile");
+        String programmeName = record.get("Programme");
+        String stageName = record.get("Stage");
+
+        // Remove the double quotes from the user profile string
+        userProfile = userProfile.replaceAll("\"", "");
+
+        // Split the user profile string into first name and last name
+        String[] nameParts = userProfile.split(",\\s*");
+        String firstName = nameParts[1]; // Index 1 contains the first name
+        String lastName = nameParts[0]; // Index 0 contains the last name
+
+        databaseManager.open();
+        // Check if the person already exists in the database
+        parameters = new HashMap<>();
+        parameters.put("firstName", firstName);
+        parameters.put("lastName", lastName);
+        List<Person> existingPeople = databaseManager.read(Person.class, "firstName = :firstName AND lastName = :lastName", parameters);
+
+        Person person;
+        if (existingPeople.isEmpty()) {
+          // Create and save the person to the database if it doesn't exist
+          person = new Person(firstName, lastName);
+          databaseManager.create(person);
+        } else {
+          // Use the existing person if it already exists
+          person = existingPeople.get(0);
+        }
+        databaseManager.update(person);
+
+        // Check if the programme already exists in the database
+        parameters = new HashMap<>();
+        parameters.put("programme", programmeName);
+        List<Programme> existingProgrammes = databaseManager.read(Programme.class, "programme = :programme", parameters);
+
+        Programme programme;
+        if (existingProgrammes.isEmpty()) {
+          // Create and save the programme to the database if it doesn't exist
+          programme = new Programme(programmeName);
+          databaseManager.create(programme);
+        } else {
+          // Use the existing programme if it already exists
+          programme = existingProgrammes.get(0);
+        }
+        databaseManager.update(programme);
+
+        // Check if the stage already exists in the database
+        parameters = new HashMap<>();
+        parameters.put("stage", stageName);
+        List<Stage> existingStages = databaseManager.read(Stage.class, "stage = :stage", parameters);
+
+        Stage stage;
+        if (existingStages.isEmpty()) {
+          // Create and save the stage to the database if it doesn't exist
+          stage = new Stage(stageName);
+          databaseManager.create(stage);
+        } else {
+          // Use the existing stage if it already exists
+          stage = existingStages.get(0);
+        }
+        databaseManager.update(stage);
+
+        databaseManager.close();
+
+        databaseManager.open();
+        // Associate the person with the programme and stage
+        if (!person.getProgrammes().contains(programme)) {
+          person.addProgramme(programme);
         }
 
-        public static void printAllPeople(List<Person> objects) {
-            System.out.println("People:");
-            for (Person obj : objects)
-                System.out.println("    " + obj.toFullString());
+        if (!person.getStages().contains(stage)) {
+          person.addStage(stage);
         }
+        databaseManager.update(person);
 
-        public static void printAllSchools(List<School> objects) {
-            System.out.println("Schools:");
-            for (School obj : objects)
-                System.out.println("    " + obj.toFullString());
-        }
+        databaseManager.close();
+      }
 
-        public static void printAllProgrammes(List<Programme> objects) {
-            System.out.println("Programmes:");
-            for (Programme obj : objects)
-                System.out.println("    " + obj.toFullString());
-        }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    }
 
-        public static void printAllStages(List<Stage> objects) {
-            System.out.println("Stages:");
-            for (Stage obj : objects)
-                System.out.println("    " + obj.toFullString());
-        }
-
-        private static void initDataset(DatabaseManager databaseManager) {
-            HashMap<String, Object> parameters;
-
-            databaseManager.open();
-            System.out.println("\n---Insert initial dataset---\n");
-            for (Person person : PEOPLE_DATA) {
-                databaseManager.create(person);
-            }
-            for (School school : SCHOOL_DATA) {
-                databaseManager.create(school);
-            }
-
-            for (Programme programme : PROGRAMME_DATA) {
-                databaseManager.create(programme);
-            }
-
-            for (Stage stage : STAGE_DATA) {
-                databaseManager.create(stage);
-            }
-
-            databaseManager.close();
-
-            databaseManager.open();
-            // Set school for harry
-            parameters = new HashMap<>();
-            parameters.put("school", "Medical School");
-            School medical = databaseManager.read(School.class, "school = :school", parameters).get(0);
-
-            parameters = new HashMap<>();
-            parameters.put("name", "Harry");
-            Person harry = databaseManager.read(Person.class, "firstName = :name", parameters).get(0);
-            harry.addSchool(medical);
-
-            // Set programme for harry
-            parameters = new HashMap<>();
-            parameters.put("programme", "CompSci");
-            Programme compsci = databaseManager.read(Programme.class, "programme = :programme", parameters).get(0);
-            harry.addProgramme(compsci);
-            databaseManager.update(harry);
-
-            databaseManager.close();
-        }
 }
